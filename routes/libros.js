71deb06b1e97e6aff5b0583c51f3b11e97c75b6f
LeonardoSantiagoG <86896647+LeonardoSantiagoG@users.noreply.github.com>
2022-05-05 22:50:45 -0500

    Update libros.js

--- routes/libros.js
+++ routes/libros.js
@@ -23,4 +23,27 @@ router.post('/:idLibro',(req,res, next)=>{
 
 
 
+router.post('/', (req,res,next)=>{
+console.log(req.body);
+var libro={
+  'id':tablaLibros2[tablaLibros2.length-1]['id']+1,
+ 'titulo':req.body.titulo,
+ 'autor':req.body.autor
+};
+tablaLibros2.push(libro);
+res.status(200).json(tablaLibros2[tablaLibros2.length-1]);
+});
+
+
+
+router.patch('/:idLibro' ,(req,res,next)=>{
+  var id=req.params.idLibro;
+
+  tablaLibros2[id-1]['titulo']=req.body.titulo;
+  tablaLibros2[id-1]['autor']=req.body.autor;
+  res.status(200).json({'mensaje':'actualizado'});
+});
+
+
+
 module.exports = router;
